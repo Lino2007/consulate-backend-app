230817e17c7f0984d49ae3b02d7cd67fa2bc2384
kkadusic <kkadusic1@etf.unsa.ba>
2021-12-27 22:33:49 +0100

    Create API for getting document if not expired

--- NSI.BusinessLogic/Interfaces/IDocumentsManipulation.cs
+++ NSI.BusinessLogic/Interfaces/IDocumentsManipulation.cs
@@ -10,5 +10,6 @@ namespace NSI.BusinessLogic.Interfaces
         Document SaveDocument(Guid requestId, Guid typeId, DateTime dateOfExpiration, string url, string title);
         Document UpdateDocument(Document document);
         Task<IList<Document>> GetDocumentsByUserIdAndType(Guid id, string type);
+        Document GetDocumentIfNotExpired(Guid documentId);
     }
 }
---
+++ NSI.REST/Controllers/DocumentController.cs
@@ -0,0 +1,50 @@
+using System;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NSI.BusinessLogic.Interfaces;
+using NSI.Common.DataContracts.Base;
+using NSI.Common.DataContracts.Enumerations;
+using NSI.DataContracts.Models;
+using NSI.REST.Filters;
+using NSI.REST.Helpers;
+
+namespace NSI.REST.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DocumentController : Controller
+    {
+        private readonly IDocumentsManipulation _documentsManipulation;
+
+        public DocumentController(IDocumentsManipulation documentsManipulation)
+        {
+            _documentsManipulation = documentsManipulation;
+        }
+
+        /// <summary>
+        /// Gets document by id, if expiration date has not passed.
+        /// </summary>
+        [Authorize]
+        [PermissionCheck("document:view")]
+        [HttpGet("{id}")]
+        public BaseResponse<Document> GetDocumentIfNotExpired([FromRoute] Guid id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return new BaseResponse<Document>
+                {
+                    Data = null,
+                    Error = ValidationHelper.ToErrorResponse(ModelState),
+                    Success = ResponseStatus.Failed
+                };
+            }
+
+            return new BaseResponse<Document>
+            {
+                Data = _documentsManipulation.GetDocumentIfNotExpired(id),
+                Error = ValidationHelper.ToErrorResponse(ModelState),
+                Success = ResponseStatus.Succeeded
+            };
+        }
+    }
+}
