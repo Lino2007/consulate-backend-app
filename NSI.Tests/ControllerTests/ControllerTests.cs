fc861c936c676892dababa96577db828203c3d2f
Lino Bevanda <lbevanda1@etf.unsa.ba>
2022-01-03 19:34:29 +0000

    Merged PR 858: Add test sln with required references
    Add test sln with required references

---
+++ NSI.Tests/ControllerTests/ControllerTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Moq;
+using NSI.BusinessLogic.Interfaces;
+using NSI.Cache.Interfaces;
+using NSI.REST.Controllers;
+using System.Threading.Tasks;
+
+namespace NSI.Tests.ControllerTests
+{
+    public class ControllerTests
+    {
+        [Fact]
+        public async Task ControllerTest1()
+        {
+            var roleMock = new Mock<IRolesManipulation>();
+            var usrPerMock = new Mock<IUserPermissionManipulation>();
+            var cacheMock = new Mock<ICacheProvider>();
+            roleMock.Setup(roleMock => roleMock.GetRolesAsync(new Guid(), null, null, null)).ReturnsAsync(() => { return null; });
+            var roleController = new RoleController(roleMock.Object, usrPerMock.Object, cacheMock.Object);
+
+            var result = await roleController.GetRoles(new DataContracts.Request.BasicRequest(), new Guid());
+
+            Assert.Null(result.Data);
+
+
+        }
+    }
+}
---
+++ NSI.Tests/ManipulationTests/ManipulationTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace NSI.Tests.ManipulationTests
+{
+    public class ManipulationTests
+    {
+        [Fact]
+        public void ManipulationTest1()
+        {
+
+        }
+    }
+}
